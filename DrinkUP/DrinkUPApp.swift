306177daeb9ed3e302ed6732f2980ac7d7bcb6fd
Harshan Dhindsa <hdhindsa@lcs.on.ca>
2025-05-20 11:36:37 -0400

    Initial Commit

---
+++ DrinkUP/DrinkUPApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkUPApp.swift
+//  DrinkUP
+//
+//  Created by Harshan Dhindsa on 2025-05-20.
+//
+
+import SwiftUI
+
+@main
+struct DrinkUPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
