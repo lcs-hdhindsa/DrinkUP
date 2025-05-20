87b732a9d955d13cf49481ed8e971d0780375de4
Harshan Dhindsa <hdhindsa@lcs.on.ca>
2025-05-20 12:17:53 -0400

    Welcome screen with "Start" button.

--- Views/StartView.swift
+++ Views/StartView.swift
@@ -9,10 +9,21 @@ import SwiftUI
 
 struct StartView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+            NavigationStack {
+                    VStack(spacing: 30) {
+                    Text("DRINKUP")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
 
+            NavigationLink("Start") {
+                        DailyIntakeView()
+                        }
+                        .buttonStyle(.borderedProminent)
+                    }
+                    .padding()
+                }
+            }
+        }
 #Preview {
     StartView()
 }
